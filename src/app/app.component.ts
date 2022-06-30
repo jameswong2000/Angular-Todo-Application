3b8c57f5c128e693f683c369f27d9fb616cbf5dd
James Wong <wongwongjames@yahoo.com.hk>
2022-06-30 11:49:01 +0800

    add to-do list layout and logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Todo-Application';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    {description: 'Eat', done: true},
+    {description: 'Sleep', done: false},
+    {description: 'Play', done: false},
+    {description: 'Laugh', done: false},
+  ];
+
+  getItems() {
+    if(this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
+  }
+
+  addItem(description: string) {
+    this.allItems.push({
+      description,
+      done:false
+    });
+  }
 }
